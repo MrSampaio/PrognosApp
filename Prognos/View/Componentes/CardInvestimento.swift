de82c9e306393744fd7d18229a1a9ecd808bcb65
marifracarolis2-arch <marifracarolis2@gmail.com>
2026-05-20 17:35:07 -0300

    Terminando a componetização do card

---
+++ Prognos/View/Componentes/CardInvestimento.swift
@@ -0,0 +1,44 @@
+//
+//  CardInvestimento.swift
+//  Prognos
+//
+//  Created by Mariana Fracaroli Lopes on 20/05/26.
+//
+
+import SwiftUI
+
+struct CardInvestimento: View {
+    let title: String
+       let subtitle: String
+       let isSelected: Bool
+       
+       var body: some View {
+           
+           VStack(spacing: 6) {
+               
+               Text(title)
+                   .font(.system(size: 22, weight: .bold))
+                   .foregroundColor(Color("CorFonte"))
+               
+               Text(subtitle)
+                   .font(.system(size: 14, weight: .medium))
+                   .foregroundColor(Color("CorFonteCard"))
+           }
+           .frame(height: 112)
+           .frame(maxWidth: .infinity)
+           .background(
+               isSelected
+               ? Color("CorSelecionado")
+               : Color("CorSecundaria")
+           )
+           .cornerRadius(24)
+           .animation(.easeInOut(duration: 0.2), value: isSelected)
+       }
+   }
+    
+    #Preview {
+        CardInvestimento( title: "CDB",
+                          subtitle: "IPCA+",
+                          isSelected: true
+                      )
+    }
