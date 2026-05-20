405381a05cdb707fd21df74afa854a0362060509
MrSampaio <jusampa2@gmail.com>
2026-05-20 16:29:41 -0300

    tela escolha

---
+++ Prognos/View/TelaEscolhaView.swift
@@ -0,0 +1,18 @@
+//
+//  TelaEscolha.swift
+//  Prognos
+//
+//  Created by Julio Sampaio on 20/05/26.
+//
+
+import SwiftUI
+
+struct TelaEscolha: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    TelaEscolha()
+}
