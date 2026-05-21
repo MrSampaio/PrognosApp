e5cd6c66ea46bca8d77ffc53189fff9d44add5e9
Leo-gsilva <leonardosilva200610@gmail.com>
2026-05-21 14:51:17 -0300

    hotfix

---
+++ Prognos/Model/CaixaTextoModel.swift
@@ -0,0 +1,20 @@
+//
+//  CaixaTextoModel.swift
+//  Prognos
+//
+//  Created by Leonardo Gonçalves da Silva on 21/05/26.
+//
+
+import SwiftUI
+
+struct CaixaTextoModel: Hashable {
+    let placeholder: String
+    var texto: String
+    let arredondamento: Int
+    
+    init(placeholder: String, texto: String, arredondamento: Int) {
+        self.placeholder = placeholder
+        self.texto = texto
+        self.arredondamento = arredondamento
+    }
+}
---
+++ Prognos/ViewModel/CaixaTextoViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  CaixaTextoViewModel.swift
+//  Prognos
+//
+//  Created by Leonardo Gonçalves da Silva on 21/05/26.
+//
+
+class CaixaTextoViewModel {
+    public static let caixaTexto = [
+        CaixaTextoModel(placeholder: "Digite o valor", texto: "", arredondamento: 20)
+    ]
+}
