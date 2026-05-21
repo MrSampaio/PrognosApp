5da85f4b538ed7f0ba24f3e30214d2b4e0074b0a
MrSampaio <jusampa2@gmail.com>
2026-05-20 23:27:08 -0300

    enum titulos e subtitulos

--- Prognos/Model/CatalogoInvestimentoModel.swift
+++ Prognos/Model/CatalogoInvestimentoModel.swift
@@ -4,41 +4,74 @@
 //
 //  Created by Julio Sampaio on 20/05/26.
 //
-
 import Foundation
 
 enum TipoDeInvestimento: String, CaseIterable, Identifiable {
     var id: String { self.rawValue }
     
     // 1. Tesouro Direto
-    case tesouroPrefixado = "Tesouro Prefixado"
-    case tesouroSelic = "Tesouro Selic"
-    case tesouroIpca = "Tesouro IPCA+"
+    case tesouroPrefixado = "tesouro_prefixado"
+    case tesouroSelic = "tesouro_selic"
+    case tesouroIpca = "tesouro_ipca"
     
     // 2. CDB / LC
-    case cdbPrefixado = "CDB/LC Prefixado"
-    case cdbCdi = "CDB/LC Pós (CDI)"
-    case cdbIpca = "CDB/LC Híbrido"
+    case cdbPrefixado = "cdb_prefixado"
+    case cdbCdi = "cdb_cdi"
+    case cdbIpca = "cdb_ipca"
     
     // 3. LCI / LCA
-    case lciPrefixado = "LCI/LCA Prefixado"
-    case lciCdi = "LCI/LCA Pós(CDI)"
-    case lciIpca = "LCI/LCA Híbrido"
+    case lciPrefixado = "lci_prefixado"
+    case lciCdi = "lci_cdi"
+    case lciIpca = "lci_ipca"
     
     // 4. Debêntures Comuns
-    case debComumPrefixada = "Deb. Comum Prefixada"
-    case debComumCdi = "Deb. Comum Pós(CDI)"
-    case debComumIpca = "Deb. Comum Híbrida"
+    case debComumPrefixada = "deb_comum_prefixada"
+    case debComumCdi = "deb_comum_cdi"
+    case debComumIpca = "deb_comum_ipca"
     
     // 5. CRI / CRA / Deb. Incentivadas
-    case isentoPrefixado = "CRI/CRA Prefixado"
-    case isentoCdi = "CRI/CRA Pós(CDI)"
-    case isentoIpca = "CRI/CRA Híbrido"
+    case isentoPrefixado = "isento_prefixado"
+    case isentoCdi = "isento_cdi"
+    case isentoIpca = "isento_ipca"
     
     // 6. Fundos
-    case fundoRendaFixa = "Fundo Renda Fixa"
+    case fundoRendaFixa = "fundo_renda_fixa"
     
-    // --- REGRAS DE INTERFACE (para definir quais inputs aparecerão)
+
+    // MARK: - REGRAS DE TEXTO PARA O CARD
+    
+    var tituloPrincipal: String {
+        switch self {
+        case .tesouroPrefixado, .tesouroSelic, .tesouroIpca: return "Tesouro"
+        case .cdbPrefixado, .cdbCdi, .cdbIpca: return "CDB / LC"
+        case .lciPrefixado, .lciCdi, .lciIpca: return "LCI / LCA"
+        case .debComumPrefixada, .debComumCdi, .debComumIpca: return "Debênture"
+        case .isentoPrefixado, .isentoCdi, .isentoIpca: return "CRI / CRA"
+        case .fundoRendaFixa: return "Fundo"
+        }
+    }
+    
+    var subtitulo: String {
+        switch self {
+        case .tesouroPrefixado, .cdbPrefixado, .lciPrefixado, .debComumPrefixada, .isentoPrefixado:
+            return "Prefixado"
+            
+        case .tesouroSelic:
+            return "Selic"
+            
+        case .cdbCdi, .lciCdi, .debComumCdi, .isentoCdi:
+            return "Pós-fixado (CDI)"
+            
+        case .tesouroIpca, .cdbIpca, .lciIpca, .debComumIpca, .isentoIpca:
+            return "Híbrido (IPCA+)"
+            
+        case .fundoRendaFixa:
+            return "Renda Fixa"
+        }
+    }
+    
+    // MARK: - REGRAS DE INTERFACE (Inputs)
+    
     var pedeTaxaPrefixada: Bool {
         switch self {
         case .tesouroPrefixado, .tesouroIpca, .cdbPrefixado, .cdbIpca, .lciPrefixado, .lciIpca, .debComumPrefixada, .debComumIpca, .isentoPrefixado, .isentoIpca: return true
@@ -64,6 +97,10 @@ enum TipoDeInvestimento: String, CaseIterable, Identifiable {
         return self == .fundoRendaFixa
     }
     
+    
+    // MARK: - MOTOR MATEMÁTICO
+
+    
     func criarInvestimento(taxaFixa: Double, percentualCDI: Double, taxaAdministracao: Double) -> Investimento {
         switch self {
             
@@ -75,7 +112,6 @@ enum TipoDeInvestimento: String, CaseIterable, Identifiable {
                 regraDeTaxasExtras: RegrasMatematicas.taxaB3()
             )
         case .tesouroSelic:
-            // Tesouro Selic rende próximo a 100% da Selic
             return Investimento(
                 regraDeRentabilidade: RegrasMatematicas.posFixado(percentual: 1.0),
                 regraDeImposto: RegrasMatematicas.tabelaRegressiva(),
@@ -139,7 +175,4 @@ enum TipoDeInvestimento: String, CaseIterable, Identifiable {
             )
         }
     }
-    
-    
-    
 }
