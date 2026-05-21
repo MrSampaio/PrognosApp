1a7179f306c1ed9e22425d5c0a63f693e69b0cbc
Leo-gsilva <leonardosilva200610@gmail.com>
2026-05-21 16:29:36 -0300

    caixas de texto COMPLETAS

--- Prognos/ViewModel/CaixaTextoViewModel.swift
+++ Prognos/ViewModel/CaixaTextoViewModel.swift
@@ -5,8 +5,10 @@
 //  Created by Leonardo Gonçalves da Silva on 21/05/26.
 //
 
+internal import CoreFoundation
 class CaixaTextoViewModel {
     public static let caixaTexto = [
-        CaixaTextoModel(placeholder: "Digite o valor", texto: "", arredondamento: 20)
+        CaixaTextoModel(placeholder: "Digite o valor", texto: "", arredondamento: 30),
+        CaixaTextoModel(placeholder: "Digite o tempo", texto: "", arredondamento: 30)
     ]
 }
