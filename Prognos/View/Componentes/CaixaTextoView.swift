88d7b6d0f943c2d41e2f1e21dc0fd0e7cf2f35c4
Leo-gsilva <leonardosilva200610@gmail.com>
2026-05-22 17:52:37 -0300

    caixas quase responsivas

--- Prognos/View/Componentes/CaixaTextoView.swift
+++ Prognos/View/Componentes/CaixaTextoView.swift
@@ -3,17 +3,18 @@ import SwiftUI
 struct CaixaTextoView: View {
     
     @Binding var caixa: CaixaTextoModel
+    @ScaledMetric(relativeTo: .body) var paddingAdaptativo: CGFloat = 10
     
     var body: some View {
         TextField("", text: $caixa.texto, prompt: Text(caixa.placeholder)
-            .font(.custom("BaiJamjuree-Medium", size: 20, relativeTo: .title))
+            .font(.custom("BaiJamjuree-Medium", size: 12, relativeTo: .title2))
             .foregroundColor(Color(.corFonte))
         )
         .textFieldStyle(.plain)
-        .font(.custom("BaiJamjuree-Medium", size: 20, relativeTo: .title))
+        .font(.custom("BaiJamjuree-Medium", size: 12, relativeTo: .title2))
         .foregroundColor(Color(.corFonte))
         .padding(.horizontal, 20)
-        .padding(.vertical)
+        .padding(.vertical, paddingAdaptativo )
         .frame(maxWidth: .infinity, alignment: .leading)
         .background(caixa.cor)
         .clipShape(RoundedRectangle(cornerRadius: caixa.arredondamento))
