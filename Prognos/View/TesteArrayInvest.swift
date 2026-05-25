cd5bcc2f70995931d41b01bb4a4829dc75e5e5fb
MrSampaio <jusampa2@gmail.com>
2026-05-25 15:38:52 -0300

    pacote de informações dos investimentos

--- Prognos/View/TesteArrayInvest.swift
+++ Prognos/View/TesteArrayInvest.swift
@@ -2,18 +2,38 @@ import SwiftUI
 
 struct TelaInformacoesView: View {
     
-    // Controles globais (Topo da tela)
     @State var valorGlobal = CaixaTextoViewModel.caixaTexto[0]
     @State var tempoGlobal = CaixaTextoViewModel.caixaTexto[1]
     
-    // Lista de ViewModels que controlarão a tela
     @State var viewModels: [CardViewModel]
+    @State private var irParaResultados = false
     
-    // O Init transforma os Enums recebidos em ViewModels prontos
     init(investimentos: [TipoDeInvestimento]) {
         let vmsMapeados = investimentos.map { CardViewModel(tipo: $0) }
         _viewModels = State(initialValue: vmsMapeados)
     }
+    
+    var pacoteDeDados: DadosDaSimulacao {
+            // Pega os valores globais
+            let stringValor = valorGlobal.texto.replacingOccurrences(of: ",", with: ".")
+            let stringTempo = tempoGlobal.texto
+            
+            let valorFim = Float(stringValor) ?? 0.0
+            let tempoFim = Int(stringTempo) ?? 0
+            
+            // Pega as taxas de dentro de CADA card
+            let investimentosProntos = viewModels.map { vm -> InvestimentoConfigurado in
+                let stringTaxa = vm.caixaTexto.texto.replacingOccurrences(of: ",", with: ".")
+                let taxaConvertida = Float(stringTaxa) ?? 0.0
+                return InvestimentoConfigurado(tipo: vm.tipo, taxaDigitada: taxaConvertida)
+            }
+            
+            return DadosDaSimulacao(
+                valorInicial: valorFim,
+                tempoAnos: tempoFim,
+                investimentos: investimentosProntos
+            )
+        }
     
     var body: some View {
         ScrollView {
@@ -38,13 +58,33 @@ struct TelaInformacoesView: View {
                 .padding(.vertical, 10)
                 
             }
+
+            let podeSimular = !valorGlobal.texto.isEmpty && !tempoGlobal.texto.isEmpty
             
-            BotaoView(texto: "Simular", habilitado: false)
+            Spacer()
+                .frame(height: 30)
+            
+            Button {
+                // Ao clicar, disparamos o gatilho da navegação
+                irParaResultados = true
+            } label: {
+                BotaoView(texto: "Simular", habilitado: podeSimular)
+            }
+            .disabled(!podeSimular)
+            .padding(.bottom, 40)
+            // 4. O GATILHO INTELIGENTE: Puxa o "pacoteDeDados" fresquinho da memória!
+            .navigationDestination(isPresented: $irParaResultados) {
+                TelaResultadosView(dados: pacoteDeDados)
+            }
+            
         }
     }
 }
 
 #Preview {
     let investimentosSimulacao = [TipoDeInvestimento.cdbPrefixado, TipoDeInvestimento.cdbCdi]
-    TelaInformacoesView(investimentos: investimentosSimulacao)
+        // Coloquei dentro de um NavigationStack no preview para o botão poder ser clicado
+        NavigationStack {
+            TelaInformacoesView(investimentos: investimentosSimulacao)
+        }
 }
